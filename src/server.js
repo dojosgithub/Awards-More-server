bd0def63ac3df2658aff3780598da3849a8aa487
FATIKAHMED <fatik.khan.200@gmail.com>
2025-04-23 19:46:49 +0500

    removed unused code

--- src/server.js
+++ src/server.js
@@ -4,27 +4,29 @@ import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 import cors from 'cors'
 import routes from './routes'
-import { errorHandler, decodeRoleTokenMiddleware } from './middlewares'
+//enable errorHandler when it is implemented
+// import { errorHandler, decodeRoleTokenMiddleware } from './middlewares'
+// import { decodeRoleTokenMiddleware } from './middlewares'
 import { connectMongoDB } from './config/dbConnection'
 import { corsConfig } from './config/cors'
-import passport from 'passport'
-import session from 'express-session'
+// import passport from 'passport'
+// import session from 'express-session'
 // import passport.js
 import './utils/passport.js'
 // import sse from './config/sse'
-import { CONTROLLER_PAYMENT } from './controllers'
+// import { CONTROLLER_PAYMENT } from './controllers'
 import fs from 'fs'
 // import secretsManager from './config/secretsManager'
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 import configSwagger from './config/swagger'
 
 import { createServer } from 'node:http'
-import { Server } from 'socket.io'
+// import { Server } from 'socket.io'
 import { init } from './socket'
 import { setupSocketEventHandlers } from './socketEvents'
 import { task } from './utils/cron'
-import { challengeTask } from './utils/challenge-cron'
-import { initializeFirebase } from './utils/firebase'
+// import { challengeTask } from './utils/challenge-cron'
+// import { initializeFirebase } from './utils/firebase'
 
 // For Socket.io
 global.serverRoot = path.resolve(__dirname)
@@ -36,7 +38,7 @@ const server = createServer(app)
 init(server)
 // Setup Socket.IO event handlers
 setupSocketEventHandlers()
-initializeFirebase()
+// initializeFirebase()
 const PORT = process.env.PORT || 3000
 const PUBLIC_PATH = path.join(__dirname, 'public')
 connectMongoDB()
@@ -45,23 +47,23 @@ app.use(express.static(PUBLIC_PATH))
 app.use(logger('dev'))
 app.use(cookieParser())
 app.use(cors(corsConfig))
-app.post(
-  '/api/payment/stripe/webhook',
-  express.raw({ type: 'application/json' }),
-  CONTROLLER_PAYMENT.stripeWebhookSecure
-)
+// app.post(
+//   '/api/payment/stripe/webhook',
+//   express.raw({ type: 'application/json' }),
+//   CONTROLLER_PAYMENT.stripeWebhookSecure
+// )
 app.use(express.json({ limit: '50mb', extended: true }))
-app.use(decodeRoleTokenMiddleware)
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET, // session secret
-    resave: false,
-    saveUninitialized: false,
-  })
-)
+// app.use(decodeRoleTokenMiddleware)
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET, // session secret
+//     resave: false,
+//     saveUninitialized: false,
+//   })
+// )
 // initialize passport and session
-app.use(passport.initialize())
-app.use(passport.session())
+// app.use(passport.initialize())
+// app.use(passport.session())
 // app.get(
 //   '/api/stream',
 //   (req, res, next) => {
@@ -93,14 +95,14 @@ app.use(passport.session())
 // )
 app.use('/api-docs', configSwagger)
 app.use('/api', routes)
-app.use(errorHandler)
+// app.use(errorHandler)
 
 app.get('/ping', (req, res) => res.send('Ping Successfulls 😄'))
 
 server.listen(PORT, async () => {
   // app.listen(PORT, '0.0.0.0', '0', async () => {
   task.start()
-  challengeTask.start()
+  // challengeTask.start()
   console.log(`[⚡️ server]: Server running on port ${PORT} | Environment: ${process.env.NODE_ENV}`)
 })
 
